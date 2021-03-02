4fa6db31b46e69946f8f5e0607566137b5f75c95
shicks <shicks255@yahoo.com>
2021-03-01 21:03:25 -0600

    prop files

--- src/main/kotlin/com/steven/hicks/lastFmService/scheduled/ScheduledDataLoader.kt
+++ src/main/kotlin/com/steven/hicks/lastFmService/scheduled/ScheduledDataLoader.kt
@@ -3,6 +3,7 @@ package com.steven.hicks.lastFmService.scheduled
 import com.steven.hicks.lastFmService.services.DataLoadService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class ScheduledDataLoader(val dataLoadService: DataLoadService) {
@@ -12,6 +13,7 @@ class ScheduledDataLoader(val dataLoadService: DataLoadService) {
 //    @Scheduled(initialDelay = 1000, fixedDelay = 100000)
     fun loadDay() {
         //log
+        println("Loading ${LocalDate.now().minusDays(1)}")
         dataLoadService.performDataLoad()
     }
 }
