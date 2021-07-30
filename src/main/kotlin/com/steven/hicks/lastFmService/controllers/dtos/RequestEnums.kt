743a330894e69794aca2ce29671ca96448900d06
shicks <shicks255@yahoo.com>
2021-07-29 22:09:41 -0500

    Query Builder stuff

--- src/main/kotlin/com/steven/hicks/lastFmService/controllers/dtos/RequestEnums.kt
+++ src/main/kotlin/com/steven/hicks/lastFmService/controllers/dtos/RequestEnums.kt
@@ -1,21 +1,11 @@
 package com.steven.hicks.lastFmService.controllers.dtos
 
-enum class SortBy(val field: String) {
-    ARTIST("artist_name"),
-    ALBUM("album_name"),
-    TIME("time");
-
-    override fun toString(): String {
-        return field
-    }
-}
-
 enum class GroupBy(val field: String) {
     ARTIST("artist_name"),
     ALBUM("album_name");
 
     override fun toString(): String {
-        return field
+        return this.name
     }
 }
 
