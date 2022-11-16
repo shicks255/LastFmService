fe106bb71954af80890ad6e6499a57604cebcb2c
shicks <shicks255@yahoo.com>
2022-11-15 19:51:08 -0600

    removing UserNameCheckingFilter.kt for now

--- src/main/kotlin/com/steven/hicks/lastFmService/controllers/filters/UserNameCheckingFilter.kt
+++ src/main/kotlin/com/steven/hicks/lastFmService/controllers/filters/UserNameCheckingFilter.kt
@@ -14,7 +14,7 @@ import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@Component
+//@Component
 class UserNameCheckingFilter(val scrobbleRepository: ScrobbleRepository, val objectMapper: ObjectMapper) : Filter {
 
     companion object {
